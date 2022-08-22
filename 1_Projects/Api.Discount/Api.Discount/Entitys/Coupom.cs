c0620b0c6592b7ce381d8e148b15e0479afbfce4
Jefferson Santos <jeff.debian2016@gmail.com>
2022-08-22 19:09:45 -0300

    Criando api discount.

---
+++ 1_Projects/Api.Discount/Api.Discount/Entitys/Coupom.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Discount.Entitys
+{
+  public class Coupon
+  {
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string Description { get; set; }
+    public int Amount { get; set; }
+  }
+}
